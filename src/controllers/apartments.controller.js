f0669f09407a6b21363e9b089a1cd452382e119f
Midedollars <olanrewajuolamide100@gmail.com>
2022-02-02 21:45:21 +0800

    feat/modified apartment controller

--- src/controllers/apartments.controller.js
+++ src/controllers/apartments.controller.js
@@ -25,3 +25,82 @@ exports.addApartments = async (req, res, next) => {
         
         }
     }
+
+
+
+    exports.changeApartments = async (req, res, next) => {
+        try {
+          const { _id } = req.params;
+          const ApartmentsUpdate = await Apartments.findOneAndUpdate({ _id: _id }, req.body, {
+            new: true,
+          });
+          return res.status(200).json({
+            success: true,
+            ApartmentsUpdate,
+          });
+        } catch (error) {
+          console.log(error);
+          return res.status(500).json({
+            success: false,
+            message: "Server Error",
+          });
+        }
+      };
+
+
+
+      
+    exports.fetchSingleApartments = async (req, res, next) => {
+        try {
+
+          const { desc } = req.params;
+          console.log(desc);
+         
+          const singleApartments = await Apartments.findOne({ emailAddress: desc });
+          if (!singleApartments) {
+            return res.status(404).json({
+              success: false,
+              message: "Apartment Not Found!",
+            });
+          }
+          return res.status(200).json({
+            success: true,
+            singleApartment,
+          });
+        } catch (error) {
+          console.log(error);
+          return res.status(500).json({
+            success: false,
+            message: "Server Error",
+          });
+        }
+      };
+
+
+
+
+    exports.fetchApartments = async (req, res, next) => {
+        try {
+          const apartments = await Apartments.find();
+          return res.status(200).json({
+            success: true,
+            apartments,
+          });
+        } catch (error) {
+          return res.status(500).json({
+            success: false,
+            message: "Server Error",
+          });
+        }
+      };
+
+
+      
+
+
+    
+      
+
+
+
+
