8e12e822b60d9cf0924741ba5b4e547b65969a87
Midedollars <olanrewajuolamide100@gmail.com>
2022-02-11 19:28:25 +0800

    feat/new

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -20,7 +20,7 @@ exports.addUsers = async (req, res, next) => {
      catch (error) {
          return res.status(500).json({
              success: false,
-             message: "error",
+             message: error.message,
          })
         
         }
